3d3afc064897bc2f78a8fe8a65959e26a1287bdd
Darius Kellermann <kellermann@protonmail.com>
2023-02-14 08:30:21 +0100

    fix some tests

--- src/m90e26_test.cpp
+++ src/m90e26_test.cpp
@@ -6,8 +6,12 @@ namespace belmoor {
 
   SCENARIO("checksum calculation", "[m90e26]") {
     GIVEN("") {
-      auto uut = M90E26_measurement_registers{0x6763U, 0x7b19U, 0x7530U, 0U, 8U};
-      REQUIRE(uut.cs2() == 0x2b0bU);
+      const auto vectors = GENERATE(
+          std::make_pair<uint16_t, M90E26_measurement_registers>(
+              0x2b0bU, {0x6763U, 0x7b19U, 0x7530U, 0U, 8U}),
+          std::make_pair<uint16_t, M90E26_measurement_registers>(
+              0xdb1bU, {0x679bU, 0x492bU, 0x7530U, 0U, 0U}));
+      REQUIRE(vectors.second.cs2() == vectors.first);
     }
   }
 
--- src/relay_test.cpp
+++ src/relay_test.cpp
@@ -5,16 +5,14 @@
 namespace belmoor {
 
   SCENARIO("", "[relay]") {
-    GIVEN("normally open, no timeout") {
-      auto uut = Relay{true, false};
+    GIVEN("normally open") {
+      auto uut = Relay{true};
 
       REQUIRE_FALSE(uut.closed());
       REQUIRE_FALSE(uut.pending());
 
       WHEN("toggling") {
         uut.toggle();
-
-        uut(Relay_Timeout + 1);
 
         THEN("nothing happens") {
           REQUIRE_FALSE(uut.closed());
@@ -22,36 +20,6 @@ namespace belmoor {
 
           WHEN("forcing the switch") {
             uut.update();
-
-            THEN("relay closes") {
-              REQUIRE(uut.closed());
-              REQUIRE_FALSE(uut.pending());
-            }
-          }
-        }
-      }
-    }
-
-    GIVEN("normally open, with timeout") {
-      auto uut = Relay{true, true};
-
-      REQUIRE_FALSE(uut.closed());
-      REQUIRE_FALSE(uut.pending());
-
-      WHEN("toggling") {
-        uut.toggle();
-
-        THEN("initially, nothing happens") {
-          REQUIRE_FALSE(uut.closed());
-          REQUIRE(uut.pending());
-
-          uut(Relay_Timeout - 1);
-
-          REQUIRE_FALSE(uut.closed());
-          REQUIRE(uut.pending());
-
-          WHEN("timeout expires") {
-            uut(1);
 
             THEN("relay closes") {
               REQUIRE(uut.closed());
